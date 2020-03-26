4dbddd978f3b921d94392f62b950161bc999a3b0
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-03-25 20:16:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+// import turotial sections
+import Welcome from './tutorial/welcome';
+import Intro from './tutorial/intro';
+import Point from './tutorial/point';
+import Line from './tutorial/line';
+import Tracking from './tutorial/tracking';
+import Area from './tutorial/area';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="container">
+			<header className="intro">Hi!</header>
+			<Welcome />
+			<Intro />
+			<div className="tutorial-steps">
+				<Point />
+				<Line />
+				<Tracking />
+				<Area />
+			</div>
+			<div className="conclusion">
+				<p>
+					For more information on editing the OpenStreetMap database,
+					please see the OSM Walkthrough [give more details].
+				</p>
+				<p>Happy mapping!</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
